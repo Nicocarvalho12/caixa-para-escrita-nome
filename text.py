58026fe337056ee9af781e3783d4f939f21bb9f9
Nicocarvalho12 <nc0454349@gmail.com>
2025-08-20 10:25:15 -0300

    Add files via upload

--- text.py
+++ text.py
@@ -3,6 +3,7 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget  
 
 class BoasVindasApp(App):
     def build(self):
@@ -31,21 +32,25 @@ class BoasVindasApp(App):
         )
         self.label.bind(size=self.label.setter('text_size'))
 
+       
+        espaco = Widget()
+
         botao = Button(
             text='Enviar',
             background_color=(2/3, 1/3, 0, 1),
             color=(1, 1, 1, 1),
             font_size=22,
-            size_hint=(1, 0.15),
-            height=60
+            size_hint=(0.4, 0.1),  
+            pos_hint={'center_x': 0.5}  
         )
 
         botao.bind(on_release=self.mostrar_mensagem)
 
         layout.add_widget(titulo)
         layout.add_widget(self.caixa_nome)
-        layout.add_widget(botao)
         layout.add_widget(self.label)
+        layout.add_widget(espaco)
+        layout.add_widget(botao)
         return layout
 
     def mostrar_mensagem(self, instance):
